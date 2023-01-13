feaa7ec333ce8262818ba418a2881f9c59755784
m4nd4r1n <kara.mandarin99@gmail.com>
2023-01-13 23:01:46 +0900

    style: Input 컴포넌트

---
+++ src/components/Input.tsx
@@ -0,0 +1,31 @@
+import type { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { cls } from '../libs/utils';
+
+interface InputProps {
+  placeholder?: string;
+  icon?: IconProp;
+}
+
+const Input = ({ icon, placeholder }: InputProps) => (
+  <div className="flex items-center w-full">
+    {icon && (
+      <FontAwesomeIcon
+        icon={icon}
+        size="xl"
+        className="text-grey absolute ml-4"
+      />
+    )}
+    <input
+      type="text"
+      className={cls(
+        'border pr-4 border-grey2 bg-transparent rounded-lg shadow-md w-full h-10 outline-none focus:border-primary-dark',
+        icon ? 'pl-12' : 'pl-4'
+      )}
+      placeholder={placeholder}
+    />
+  </div>
+);
+
+export default Input;
