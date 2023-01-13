ed2d49d8b500b41637cce91f540ff578f7b10e97
m4nd4r1n <kara.mandarin99@gmail.com>
2023-01-13 22:32:33 +0900

    chore: 사용할 색상 설정

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      red: '#F45452',
+      white: '#FFFFFF',
+      black: '#222222',
+      grey: '#888888',
+      grey2: '#D7D7D7',
+      primary: '#00D6A2',
+      'primary-dark': '#00C192',
+      'primary-light': '#33DEB5',
+      'primary-lighter': '#96E5D2',
+      transparent: 'transparent',
+    },
   },
   plugins: [],
 };
