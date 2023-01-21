f0b270c5b5013d9d16a2d1166817eb4441b11385
m4nd4r1n <kara.mandarin99@gmail.com>
2023-01-21 17:58:41 +0900

    feat: Button 컴포넌트 className prop 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,10 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 
+import { cls } from '../libs/utils';
+
 const Button = ({
   children,
+  className,
   ...rest
 }: DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
@@ -9,7 +12,10 @@ const Button = ({
 >) => (
   <button
     {...rest}
-    className="rounded-lg text-white bg-primary px-2 shadow-md disabled:shadow-none disabled:text-grey hover:bg-primary-light transition-all focus:outline focus:-outline-offset-2 focus:outline-2 focus:outline-primary-dark disabled:bg-primary-lighter disabled:cursor-not-allowed"
+    className={cls(
+      'rounded-lg text-white bg-primary px-2 py-2 shadow-md disabled:shadow-none disabled:text-grey hover:bg-primary-light transition-all focus:outline focus:-outline-offset-2 focus:outline-2 focus:outline-primary-dark disabled:bg-primary-lighter disabled:cursor-not-allowed',
+      className || ''
+    )}
   >
     {children}
   </button>
