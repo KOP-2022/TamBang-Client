ab32e7647201791e5a466b27afe8543cf297d67c
m4nd4r1n <kara.mandarin99@gmail.com>
2023-01-25 18:09:43 +0900

    feat: 회원가입 페이지
    - 회원가입 페이지 UI
    - 입력 form 전송

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 import MapPage from './pages/MapPage';
+import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />}></Route>
       <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/register" element={<RegisterPage />}></Route>
       <Route path="/map" element={<MapPage />}></Route>
     </Routes>
   );
