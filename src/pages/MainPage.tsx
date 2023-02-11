f26dc96f8e4d509129c90405b607bab987260cdf
m4nd4r1n <kara.mandarin99@gmail.com>
2023-02-11 19:28:42 +0900

    feat: 레이아웃 컴포넌트 헤더 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,19 @@
 import { useNavigate } from 'react-router-dom';
 
+import Layout from '@/components/Layout';
+
 const MainPage = () => {
   const navigate = useNavigate();
   return (
-    <>
-      <button onClick={() => navigate('/map')}>Go to map page.</button>
-    </>
+    <Layout main>
+      <div className="flex flex-col pb-4">
+        <button onClick={() => navigate('/map')}>지도 페이지</button>
+        <button onClick={() => navigate('/login')}>로그인 페이지</button>
+        <button onClick={() => navigate('/room/upload')}>
+          매물 등록 페이지
+        </button>
+      </div>
+    </Layout>
   );
 };
 
--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,13 +1,13 @@
-import CheckBox from '../components/CheckBox';
-import Layout from '../components/Layout';
-import Map from '../components/Map';
-import SearchBar from '../components/SearchBar';
+import CheckBox from '@/components/CheckBox';
+import Layout from '@/components/Layout';
+import Map from '@/components/Map';
+import SearchBar from '@/components/SearchBar';
 
 const FILTERS = ['편의점', '세탁소', '병원', '마트'] as const;
 
 const MapPage = () => {
   return (
-    <Layout>
+    <Layout title="지도">
       <div className="absolute z-10 w-full bg-white p-8">
         <SearchBar />
         <div className="flex w-full justify-around mt-8">
