e00f85a561f2ace9263afbbb3c7edca54c098e76
m4nd4r1n <kara.mandarin99@gmail.com>
2023-02-18 19:39:13 +0900

    refactor: 지도 react-kakao-maps-sdk 사용

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,42 +1,41 @@
-import { useEffect, useRef } from 'react';
+import {
+  useInjectKakaoMapApi,
+  Map as KakaoMap,
+  MapMarker,
+} from 'react-kakao-maps-sdk';
 
-declare global {
-  interface Window {
-    kakao: any;
-  }
+import type { LatLng } from 'kakao-maps';
+
+interface MapProps {
+  rooms?: LatLng[];
+  filters: string[];
 }
 
-const Map = () => {
-  const mapRef = useRef<HTMLDivElement>(null);
+const Map = ({ rooms, filters }: MapProps) => {
+  const { loading } = useInjectKakaoMapApi({
+    appkey: import.meta.env.VITE_KAKAO_MAP_KEY,
+  });
 
-  useEffect(() => {
-    const mapScript = document.createElement('script');
-    mapScript.type = 'text/javascript';
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${
-      import.meta.env.VITE_KAKAO_MAP_KEY
-    }&autoload=false`;
-    document.head.appendChild(mapScript);
+  const onMarketClick = (markerPos: LatLng) => async () => {
+    console.log('search position:', markerPos);
+    console.log('with filters:', filters);
+  };
 
-    const onLoad = () => {
-      window.kakao.maps.load(() => {
-        const { LatLng, Map, Marker } = window.kakao.maps;
-        const options = {
-          center: new LatLng(33.450701, 126.570667),
-          level: 3,
-        };
-        const map = new Map(mapRef.current, options);
-        const markerPosition = new LatLng(33.450701, 126.570667);
-        const marker = new Marker({ position: markerPosition });
-        marker.setMap(map);
-      });
-    };
-    mapScript.addEventListener('load', onLoad);
-    return () => {
-      mapScript.removeEventListener('load', onLoad);
-      document.head.removeChild(mapScript);
-    };
-  }, []);
-  return <div ref={mapRef} className="h-full"></div>;
+  return !loading ? (
+    <KakaoMap
+      center={{ lat: 33.450701, lng: 126.570667 }}
+      level={3}
+      className="h-full"
+    >
+      {rooms?.map((position, index) => (
+        <MapMarker
+          position={position}
+          key={index}
+          onClick={onMarketClick(position)}
+        ></MapMarker>
+      ))}
+    </KakaoMap>
+  ) : null;
 };
 
 export default Map;
