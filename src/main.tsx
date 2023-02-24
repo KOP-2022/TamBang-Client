c25ffa2cbbb322206fbc4b5adb945d7b6d01be8d
m4nd4r1n <kara.mandarin99@gmail.com>
2023-02-24 21:07:33 +0900

    feat: ReactQueryDevtools 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App';
 import icons from './icons';
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
       <BrowserRouter>
         <App />
+        <ReactQueryDevtools />
       </BrowserRouter>
     </React.StrictMode>
   </QueryClientProvider>
