07d9ff5261ef8c5f1087e055fc374acf21fd02ac
m4nd4r1n <kara.mandarin99@gmail.com>
2023-03-19 20:48:26 +0900

    feat: 헤더 로그인, 로그아웃 버튼 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,11 @@ import { useNavigate } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { useAtom } from 'jotai';
+import Cookies from 'universal-cookie';
+
+import { tokenAtom } from '@/atoms/token';
+
 interface HeaderProps {
   back?: boolean;
   title?: string;
@@ -10,12 +15,21 @@ interface HeaderProps {
 
 const Header = ({ back, title }: HeaderProps) => {
   const navigate = useNavigate();
+  const [token, setToken] = useAtom(tokenAtom);
 
   const onBackClick = () => {
     navigate(-1);
   };
   const onHomeClick = () => {
     navigate('/');
+  };
+  const onLoginClick = () => {
+    navigate('/login');
+  };
+  const onLogoutClick = () => {
+    const cookies = new Cookies();
+    cookies.remove('jwt');
+    setToken('');
   };
   return (
     <nav className="fixed top-0 left-0 right-0 w-full mx-auto max-w-lg z-20">
@@ -28,7 +42,7 @@ const Header = ({ back, title }: HeaderProps) => {
             {back ? (
               <FontAwesomeIcon icon={['fas', 'chevron-left']} size="xl" />
             ) : (
-              <span className="select-none font-bold text-primary text-xl">
+              <span className="select-none font-bold text-primary text-2xl">
                 TamBang
               </span>
             )}
@@ -39,9 +53,17 @@ const Header = ({ back, title }: HeaderProps) => {
           {title}
         </div>
         <div className="absolute top-0 bottom-0 right-3">
-          {back && (
+          {back ? (
             <button className="h-14 w-12" onClick={onHomeClick}>
               <FontAwesomeIcon icon={['fas', 'house']} size="xl" />
+            </button>
+          ) : token ? (
+            <button className="h-14 w-12" onClick={onLogoutClick}>
+              <FontAwesomeIcon icon="arrow-right-from-bracket" size="xl" />
+            </button>
+          ) : (
+            <button className="h-14 w-12" onClick={onLoginClick}>
+              <FontAwesomeIcon icon="arrow-right-to-bracket" size="xl" />
             </button>
           )}
         </div>
