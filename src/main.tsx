1907f9dfb32f42cef584164864e1d69e63e207ca
m4nd4r1n <kara.mandarin99@gmail.com>
2023-03-19 20:00:01 +0900

    feat: jotai devtool 적용

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,16 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import { DevTools } from 'jotai-devtools';
+
 import App from './App';
 import icons from './icons';
 import { worker } from './mocks/browser';
 
 import './index.css';
 
-if (import.meta.env.MODE === 'mocking') worker.start();
+if (import.meta.env.MODE === 'mocking')
+  worker.start({ onUnhandledRequest: 'bypass' });
 
 library.add(...icons);
 const queryClient = new QueryClient();
@@ -21,6 +24,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <React.StrictMode>
       <BrowserRouter>
+        <DevTools />
         <App />
         <ReactQueryDevtools />
       </BrowserRouter>
